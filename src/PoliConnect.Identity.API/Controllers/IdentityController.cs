c501f4ad11d4906e5483608903fe25d223e02630
NikDevRych <nikdevrych@gmail.com>
2024-10-08 14:58:40 +0300

    #2 implement identity service registration in identity controller

--- src/PoliConnect.Identity.API/Controllers/IdentityController.cs
+++ src/PoliConnect.Identity.API/Controllers/IdentityController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PoliConnect.Identity.Application.DTOs;
+using PoliConnect.Identity.Application.Enums.Results;
+using PoliConnect.Identity.Application.Services.Interfaces;
 
 namespace PoliConnect.Identity.API.Controllers;
 
@@ -7,8 +9,29 @@ namespace PoliConnect.Identity.API.Controllers;
 [ApiController]
 public class IdentityController : ControllerBase
 {
-    public Task<IActionResult> Register(RegisterDTO registerDTO)
-    {
+    private readonly IIdentityService _identityService;
 
+    public IdentityController(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<IActionResult> Register(RegisterDTO registerDTO)
+    {
+        var result = await _identityService.RegisterAsync(registerDTO);
+
+        if (!result.IsSuccess) HandleRegisterError(result.Error);
+
+        return Created();
+    }
+
+    private IActionResult HandleRegisterError(RegisterError error)
+    {
+        return error switch
+        {
+            RegisterError.EmailExist => Conflict("Email already exists"),
+            RegisterError.PhoneNumberExist => Conflict("PhoneNumber already exists"),
+            _ => throw new InvalidOperationException($"Can not handle {nameof(RegisterError)}"),
+        };
     }
 }
