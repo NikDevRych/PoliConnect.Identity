a261caf7f6a111a8231ee8426dd9f85163e49e54
NikDevRych <nikdevrych@gmail.com>
2024-10-08 13:19:01 +0300

    #2 add Result pattern class for use

---
+++ src/PoliConnect.Identity.Application/Patterns/Result.cs
@@ -0,0 +1,49 @@
+﻿namespace PoliConnect.Identity.Application.Patterns;
+
+public class Result<TError>
+{
+    protected readonly TError? _error;
+
+    public bool IsSuccess { get; }
+
+    public TError Error => !IsSuccess
+        ? _error!
+        : throw new InvalidOperationException("Can not get Error if Result is Success");
+
+    protected Result()
+    {
+        _error = default;
+        IsSuccess = true;
+    }
+
+    protected Result(TError error)
+    {
+        _error = error;
+        IsSuccess = false;
+    }
+
+    public static Result<TError> Success() => new();
+    public static Result<TError> Failure(TError error) => new(error);
+}
+
+public sealed class Result<TValue, TError> : Result<TError>
+{
+    private readonly TValue? _value;
+
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("Can not get Value if Result is not Success");
+
+    private Result(TValue value) : base()
+    {
+        _value = value;
+    }
+
+    private Result(TError error) : base(error)
+    {
+        _value = default;
+    }
+
+    public static Result<TValue, TError> Success(TValue value) => new(value);
+    public static new Result<TValue, TError> Failure(TError error) => new(error);
+}
